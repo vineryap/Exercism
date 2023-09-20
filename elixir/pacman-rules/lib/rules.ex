677c4905078ce730018010e2d702d29a38d4474a
Vin E. R. Yap <vineryap@gmail.com>
2023-09-21 07:45:24 +0900

    1st solution

--- elixir/pacman-rules/lib/rules.ex
+++ elixir/pacman-rules/lib/rules.ex
@@ -1,17 +1,13 @@
 defmodule Rules do
-  def eat_ghost?(power_pellet_active?, touching_ghost?) do
-    # Please implement the eat_ghost?/2 function
-  end
+  def eat_ghost?(power_pellet_active?, touching_ghost?),
+    do: power_pellet_active? and touching_ghost?
 
-  def score?(touching_power_pellet?, touching_dot?) do
-    # Please implement the score?/2 function
-  end
+  def score?(touching_power_pellet?, touching_dot?),
+    do: touching_power_pellet? or touching_dot?
 
-  def lose?(power_pellet_active?, touching_ghost?) do
-    # Please implement the lose?/2 function
-  end
+  def lose?(power_pellet_active?, touching_ghost?),
+    do: eat_ghost?(!power_pellet_active?, touching_ghost?)
 
-  def win?(has_eaten_all_dots?, power_pellet_active?, touching_ghost?) do
-    # Please implement the win?/3 function
-  end
+  def win?(has_eaten_all_dots?, power_pellet_active?, touching_ghost?),
+    do: has_eaten_all_dots? and !lose?(power_pellet_active?, touching_ghost?)
 end
