37aa28c2daa4c4c90aac8867414b8b1ff3135d11
Vin E. R. Yap <vineryap@gmail.com>
2023-09-21 18:37:31 +0900

    fix: functions' spec

--- elixir/language-list/lib/language_list.ex
+++ elixir/language-list/lib/language_list.ex
@@ -2,18 +2,18 @@ defmodule LanguageList do
   @spec new() :: []
   def new(), do: []
 
-  @spec add([string], string) :: [string]
+  @spec add(charlist, String.t()) :: charlist
   def add(list, language), do: [language | list]
 
-  @spec remove([string]) :: [string]
+  @spec remove(charlist) :: charlist
   def remove(list), do: tl(list)
 
-  @spec first([string]) :: [string]
+  @spec first(charlist) :: charlist
   def first(list), do: hd(list)
 
-  @spec count([string]) :: integer
+  @spec count(charlist) :: integer
   def count(list), do: length(list)
 
-  @spec functional_list?([string]) :: boolean
+  @spec functional_list?(charlist) :: boolean
   def functional_list?(list), do: "Elixir" in list
 end
