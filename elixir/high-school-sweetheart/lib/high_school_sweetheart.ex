25de82d6d10597ebbbdeb3980c92dd21f2b7be88
Vin E. R. Yap <vineryap@gmail.com>
2023-09-23 00:54:14 +0900

    solution

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,32 @@
 defmodule HighSchoolSweetheart do
-  def first_letter(name) do
-    # Please implement the first_letter/1 function
-  end
+  def first_letter(name), do: name |> String.trim() |> String.first()
 
-  def initial(name) do
-    # Please implement the initial/1 function
-  end
+  def initial(name),
+    do:
+      first_letter(name)
+      |> String.upcase()
+      |> Kernel.<>(".")
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [first, last] = String.split(full_name, " ")
+    "#{first |> initial()} #{last |> initial()}"
   end
 
-  def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
-    # Please implement the pair/2 function
-  end
+  def pair(full_name1, full_name2),
+    do: """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
 end
