aa5a3de7757df9673eb4f8fea4551df764b97b1d
Vin E. R. Yap <vineryap@gmail.com>
2023-09-24 15:24:14 +0900

    solution

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,26 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  @spec sanitize(charlist()) :: charlist()
+  def sanitize(~c""), do: ~c""
 
-    # Please implement the sanitize/1 function
+  def sanitize([first_char_code | rest]) do
+    sanitised_char =
+      case first_char_code do
+        # accept lowercase letters (a-z letters are sequentially ordered)
+        char when char >= ?a and char <= ?z -> [char]
+        # allow underscores
+        ?_ -> ~c"_"
+        # ä becomes ae
+        ?ä -> ~c"ae"
+        # ü becomes ue
+        ?ü -> ~c"ue"
+        # ö becomes oe
+        ?ö -> ~c"oe"
+        # ß becomes ss
+        ?ß -> ~c"ss"
+        # remove anything else
+        _ -> ~c""
+      end
+
+    sanitised_char ++ sanitize(rest)
   end
 end
