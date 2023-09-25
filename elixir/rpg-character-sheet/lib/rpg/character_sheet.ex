d67188583eb36da68296edc614195321ee3159f2
Vin E. R. Yap <vineryap@gmail.com>
2023-09-25 19:49:33 +0900

    solution

--- elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
+++ elixir/rpg-character-sheet/lib/rpg/character_sheet.ex
@@ -1,21 +1,24 @@
 defmodule RPG.CharacterSheet do
-  def welcome() do
-    # Please implement the welcome/0 function
-  end
+  @spec welcome :: :ok
+  def welcome(), do: IO.puts("Welcome! Let's fill out your character sheet together.")
 
-  def ask_name() do
-    # Please implement the ask_name/0 function
-  end
+  @spec ask_name :: String.t()
+  def ask_name(), do: IO.gets("What is your character's name?\n") |> String.trim()
 
-  def ask_class() do
-    # Please implement the ask_class/0 function
-  end
+  @spec ask_class :: String.t()
+  def ask_class(), do: IO.gets("What is your character's class?\n") |> String.trim()
 
-  def ask_level() do
-    # Please implement the ask_level/0 function
-  end
+  @spec ask_level :: integer()
+  def ask_level(),
+    do: IO.gets("What is your character's level?\n") |> String.trim() |> String.to_integer()
 
   def run() do
-    # Please implement the run/0 function
+    welcome()
+
+    %{}
+    |> Map.put(:name, ask_name())
+    |> Map.put(:class, ask_class())
+    |> Map.put(:level, ask_level())
+    |> IO.inspect(label: "Your character")
   end
 end
