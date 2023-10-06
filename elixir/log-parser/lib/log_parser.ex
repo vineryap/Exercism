28edfa9fca89623fa830d273bd4caaae0a869d5a
Vin E. R. Yap <vineryap@gmail.com>
2023-10-06 22:53:38 +0900

    solution

--- elixir/log-parser/lib/log_parser.ex
+++ elixir/log-parser/lib/log_parser.ex
@@ -1,17 +1,16 @@
 defmodule LogParser do
-  def valid_line?(line) do
-    # Please implement the valid_line?/1 function
-  end
+  def valid_line?(line), do: line =~ ~r/^\[(DEBUG|INFO|WARNING|ERROR)\]/
 
-  def split_line(line) do
-    # Please implement the split_line/1 function
-  end
+  def split_line(line), do: String.split(line, ~r{\<[~*=-]*\>})
 
-  def remove_artifacts(line) do
-    # Please implement the remove_artifacts/1 function
-  end
+  def remove_artifacts(line),
+    do: String.replace(line, ~r{end-of-line\d+}i, "", global: true)
 
-  def tag_with_user_name(line) do
-    # Please implement the tag_with_user_name/1 function
-  end
+  def tag_with_user_name(line),
+    do:
+      line
+      |> String.replace(
+        ~r/\[(DEBUG|INFO|WARNING|ERROR|WARN)\](.+)User[\s\\t\\n]+([^\s\\t\\n]+)/,
+        "[USER] \\g{3} \\0"
+      )
 end
