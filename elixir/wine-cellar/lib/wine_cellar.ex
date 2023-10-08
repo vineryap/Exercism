1063a65f404e10aa2558007c3fafa73d897d3afc
Vin E. R. Yap <vineryap@gmail.com>
2023-10-08 20:39:47 +0900

    solution

--- elixir/wine-cellar/lib/wine_cellar.ex
+++ elixir/wine-cellar/lib/wine_cellar.ex
@@ -1,11 +1,20 @@
 defmodule WineCellar do
-  def explain_colors do
-    # Please implement the explain_colors/0 function
-  end
+  def explain_colors,
+    do: [
+      white: "Fermented without skin contact.",
+      red: "Fermented with skin contact using dark-colored grapes.",
+      rose: "Fermented with some skin contact, but not enough to qualify as a red wine."
+    ]
 
-  def filter(cellar, color, opts \\ []) do
-    # Please implement the filter/3 function
-  end
+  def filter(cellar, color, opts \\ [])
+
+  def filter(cellar, color, []), do: Keyword.get_values(cellar, color)
+
+  def filter(cellar, color, [{:year, year} | rest_opts]),
+    do: filter(cellar, color, rest_opts) |> filter_by_year(year)
+
+  def filter(cellar, color, [{:country, country} | rest_opts]),
+    do: filter(cellar, color, rest_opts) |> filter_by_country(country)
 
   # The functions below do not need to be modified.
 
